dc93e5ea8b4c7c757d4ff7cfb9fd212ccac344ae
M-Stable <fchanghlee@gmail.com>
2020-07-15 16:32:54 +1200

    Checkbox filled

--- src/components/FilterBox.js
+++ src/components/FilterBox.js
@@ -7,6 +7,7 @@ import FormLabel from '@material-ui/core/FormLabel';
 import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from "@material-ui/core/Typography";
+import clsx from 'clsx';
 
 const useStyles = makeStyles({
     heading: {
@@ -30,19 +31,59 @@ const useStyles = makeStyles({
         '&:hover': {
             boxShadow: "none",
         },
+    },
+    root: {
+        '&:hover': {
+            backgroundColor: 'transparent',
+        },
+    },
+    icon: {
+        borderRadius: 3,
+        width: 20,
+        height: 20,
+        boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+        backgroundColor: '#f5f8fa',
+        'input:hover ~ &': {
+            backgroundColor: '#ebf1f5',
+        },
+        'input:disabled ~ &': {
+            boxShadow: 'none',
+            background: 'rgba(206,224,213,0.5)',
+        },
+    },
+    checkedIcon: {
+        backgroundColor: '#21CE99',
+        'input:hover ~ &': {
+            backgroundColor: '#21CE99',
+        },
     }
 });
 
+function StyledCheckbox(props) {
+    const classes = useStyles();
+
+    return (
+        <Checkbox
+            className={classes.root}
+            disableRipple
+            color="primary"
+            checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
+            icon={<span className={classes.icon} />}
+            inputProps={{ 'aria-label': 'decorative checkbox' }}
+            {...props}
+        />
+    );
+}
 
 function FilterBox() {
     const classes = useStyles();
-    const topics = ["Accounting", "EV /Equity Value", "Valuation", "Discounted Cash Flow", "Merger Model", "Leveraged buy-out"];
+    const topics = ["Accounting", "EV / Equity Value", "Valuation", "Discounted Cash Flow", "Merger Model", "Leveraged buy-out"];
     const difficulties = ["Easy", "Medium", "Hard"];
 
     const topicCheckBoxes = topics.map((topic) => (
         <FormControlLabel
             value={topic}
-            control={<Checkbox color="primary" />}
+            control={<StyledCheckbox />}
             label={<Typography className={classes.label}>{topic}</Typography>}
             labelPlacement="end"
             key={topic}
@@ -53,7 +94,7 @@ function FilterBox() {
         <FormControlLabel
             className={classes.label}
             value={difficulty}
-            control={<Checkbox color="primary" />}
+            control={<StyledCheckbox />}
             label={<Typography className={classes.label}>{difficulty}</Typography>}
             labelPlacement="end"
             key={difficulty}
