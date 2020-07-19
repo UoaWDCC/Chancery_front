e717452a7567803162ae621710e769e5de23d9cb
justinkrojos <justinkrojos@gmail.com>
2020-07-20 01:40:03 +1200

    Implemented basic header

--- src/pages/saved.js
+++ src/pages/saved.js
@@ -3,9 +3,38 @@ import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import { Button } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
+import {Link} from "react-router-dom";
 
 const useStyles = makeStyles({
+
+    
+    title: {
+        fontSize: '40px',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        margin: 'auto 0',
+    },
+    button: {
+        borderRadius: '5px',
+        color: 'white',
+        fontSize: '25px',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        boxShadow: 'none',
+        
+        width: '350px',
+        height: '80px',
+
+        '&:hover': {
+            borderWidth: '3px',
+            backgroundColor: '#ffffff',
+            color: '#21CE99',
+            boxShadow: 'none',
+        },
+    },
     
 })
 
@@ -17,26 +46,40 @@ function Saved() {
            <Container
                 container
                 spacing={0}
-                style={{padding: '3em 10em 4em 10em', border: '2px solid black'}}
+                style={{padding: '3em 3em 4em 3em', border: '2px solid black'}}
             >
 
-                <Container style={{border: '2px solid black', height: 80}}>
-                    <Typography style={{fontSize: 40, fontWeight: 'bold'}}>SAVED QUESTIONS:</Typography>
-                </Container>
+                <Grid container>
+                    <Grid item container xs={1}>
+                    </Grid>
+                    <Grid item container xs={6}>
+                        <Typography className={classes.title}>Saved Questions:</Typography>
+                    </Grid>
+                    <Grid item container xs={5} justify="flex-end">
+                        <Link to={"/revise"} style={{textDecoration: "none"}}>
+                            <Button className={classes.button} color={"primary"} variant={"contained"}>Practice Now</Button>
+                        </Link>
+                    </Grid>
 
-                <Container style={{border: '2px solid black', height: 80}}>
-                    1
-                </Container>
+                </Grid>
 
-                <Container style={{border: '2px solid black', height: 80}}>
+                <Grid container style={{border: '2px solid red'}}>
+                    <Grid item container xs={1}>
+                    </Grid>
+                    <Grid item container xs={11}>
+                        <Typography className={classes.title}>Saved Questions:</Typography>
+                    </Grid>
+                </Grid>
+
+                <Container style={{border: '2px solid black', height: 200}}>
                     2
                 </Container>
 
-                <Container style={{border: '2px solid black', height: 80}}>
+                <Container style={{border: '2px solid black', height: 200}}>
                     3
                 </Container>
 
-                <Container style={{border: '2px solid black', height: 80}}>
+                <Container style={{border: '2px solid black', height: 200}}>
                     4
                 </Container>
                 
