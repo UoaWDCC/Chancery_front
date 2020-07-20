bd86e5b5be254006e108acf725c9f27a8b77463f
M-Stable <fchanghlee@gmail.com>
2020-07-20 15:34:04 +1200

    remove Saved Flashcard box shadow on dark mode

--- src/components/SavedFlashcard.js
+++ src/components/SavedFlashcard.js
@@ -18,7 +18,7 @@ const useStyles = makeStyles( theme => ({
     flashcardBackground: {
         backgroundColor: theme.palette.type === "dark" ? '#5F5F5F' : '#F5F5F5',
         borderRadius: '10px',
-        boxShadow: '0 0 5px 0 grey',
+        boxShadow: theme.palette.type === "dark" ? 'none' : '0 0 5px 0 grey',
         padding: '20px 40px 20px 40px',
     },
     subheading: {
