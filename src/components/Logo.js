1f5502086eb57262f181a8e30d422b2175c76103
M-Stable <fchanghlee@gmail.com>
2020-07-20 13:56:57 +1200

    Finish logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {makeStyles, useTheme} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import LogoIcon from '../icons/Chancery_logo_white.png';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
     logo: {
@@ -9,27 +11,44 @@ const useStyles = makeStyles((theme) => ({
         borderRadius: 7,
         backgroundColor: "#21CE99",
     },
-
     logoDiv: {
         backgroundColor: theme.palette.type === 'dark' ? "#313131": "#21CE99",
-        width: 200,
+        width: 220,
         height: 60,
         borderRadius: 7,
         marginTop: 10,
         marginLeft: 10,
         color: theme.palette.type === 'dark' ? "#21CE99": "#FFFFFF",
     },
+    logoText: {
+        textTransform: "uppercase",
+        fontSize: 24,
+        paddingLeft: 10,
+        paddingRight: 10,
+    }
 }));
 
 function Logo() {
     const classes = useStyles();
-    const theme = useTheme();
-    const isDark = (theme.palette.type === 'dark');
+
     return (
-        <div className={classes.logoDiv}>
-            <img src={LogoIcon} className={classes.logo} alt="Logo"/>
-            CHANCERY
-        </div>
+        <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="center"
+            className={classes.logoDiv}
+        >
+            <Grid item>
+                <img src={LogoIcon} className={classes.logo} alt="Logo"/>
+            </Grid>
+            <Grid item>
+                <Typography className={classes.logoText}>
+                    Chancery
+                </Typography>
+            </Grid>
+
+        </Grid>
     );
 }
 
