3dd2f6b49e8944267876ff0fc74803d6f762785a
M-Stable <fchanghlee@gmail.com>
2020-07-22 21:43:33 +1200

    Menu button on small screen Filter button for revise page on small screen

--- src/App.js
+++ src/App.js
@@ -14,6 +14,19 @@ import {ThemeProvider} from "@material-ui/styles";
 import Logo from "./components/Logo";
 import { useDispatch } from 'react-redux';
 import { fetchQuestions } from './redux/actions';
+import Drawer from "@material-ui/core/Drawer";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import List from "@material-ui/core/List";
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from '@material-ui/icons/Home';
+import BookIcon from '@material-ui/icons/Book';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
+import { useMediaQuery } from 'react-responsive'
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ToggleOnIcon from "@material-ui/icons/ToggleOn";
+import ToggleOffIcon from "@material-ui/icons/ToggleOff";
 
 const StyledTabs = withStyles((theme) => ({
     indicator: {
@@ -29,8 +42,8 @@ const StyledTabs = withStyles((theme) => ({
     },
     root: {
         float: 'right',
-        paddingTop: '15px',
         paddingRight: '20px',
+        paddingTop: '10px',
     }
 }))((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
 
@@ -64,6 +77,17 @@ const useStyles = makeStyles(() => ({
         marginTop: 10,
         marginLeft: 10,
     },
+    menuButton: {
+        position: "fixed",
+        top: 0,
+        left: 0,
+    },
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
 }));
 
 function App() {
@@ -71,7 +95,16 @@ function App() {
     const allTabs = ['/', '/revise', '/saved'];
     const [anchorEl, setAnchorEl] = useState(null);
     const [darkMode, setDarkMode] = useState(false);
+    const [state, setState] = useState({left: false});
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1280px)' })
 
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
 
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -117,22 +150,52 @@ function App() {
                                 path="/"
                                 render={({ location }) => (
                                     <Fragment>
-                                        <Grid container style={{ minHeight: '10vh' }}>
-                                            <Grid item xs>
-                                                <Logo/>
-                                            </Grid>
-                                            <Grid item xs>
-                                                <div className={classes.nav}>
-                                                    <StyledTabs value={location.pathname} aria-label="styled tabs example">
-                                                        <StyledTab label="Home" value="/" component={Link} to={allTabs[0]} />
-                                                        <StyledTab label="Revise" value="/revise" component={Link} to={allTabs[1]} />
-                                                        <StyledTab label="Saved" value="/saved" component={Link} to={allTabs[2]} />
-                                                        <StyledTab label="My Account" onClick={handleClick} />
-                                                        <AccountMenu anchorEl={anchorEl} onClose={handleClose} setDarkMode={setDarkMode} darkMode={darkMode}/>
-                                                    </StyledTabs>
-                                                </div>
-                                            </Grid>
-                                        </Grid>
+                                        {isTabletOrMobile ?
+                                            <Fragment key={'left'}>
+                                                <IconButton className={classes.menuButton} onClick={toggleDrawer('left', true)}><MenuIcon/></IconButton>
+                                                <Drawer className={classes.drawer} anchor={'left'} open={state['left']} onClose={toggleDrawer('left', false)}>
+                                                    <div
+                                                        className={classes.list}
+                                                        onClick={toggleDrawer('left', false)}
+                                                        onKeyDown={toggleDrawer('left', false)}
+                                                    >
+                                                        <List>
+                                                            <ListItem button value="/" component={Link} to={allTabs[0]}>
+                                                                <ListItemIcon><HomeIcon color={"primary"}/></ListItemIcon>
+                                                                <ListItemText primary={'Home'}/>
+                                                            </ListItem>
+                                                            <ListItem button value="/revise" component={Link} to={allTabs[1]}>
+                                                                <ListItemIcon><BookIcon color={"primary"}/></ListItemIcon>
+                                                                <ListItemText primary={'Revise'} />
+                                                            </ListItem>
+                                                            <ListItem button value="/saved" component={Link} to={allTabs[2]}>
+                                                                <ListItemIcon><BookmarksIcon color={"primary"}/></ListItemIcon>
+                                                                <ListItemText primary={'Saved'} />
+                                                            </ListItem>
+                                                            <ListItem button onClick={() => setDarkMode(!darkMode)}>
+                                                                <ListItemIcon>{darkMode ? <ToggleOnIcon color={"primary"} fontSize="small" /> : <ToggleOffIcon color={"primary"} fontSize="small" />}</ListItemIcon>
+                                                                {darkMode ? <ListItemText primary="Light Mode" /> : <ListItemText primary="Dark Mode" />}
+                                                            </ListItem>
+                                                        </List>
+                                                    </div>
+                                                </Drawer>
+                                            </Fragment> :
+                                            <Grid container style={{ minHeight: '10vh' }}>
+                                                <Grid item xs>
+                                                    <Logo/>
+                                                </Grid>
+                                                <Grid item xs>
+                                                    <div className={classes.nav}>
+                                                        <StyledTabs value={location.pathname} aria-label="styled tabs example">
+                                                            <StyledTab label="Home" value="/" component={Link} to={allTabs[0]} />
+                                                            <StyledTab label="Revise" value="/revise" component={Link} to={allTabs[1]} />
+                                                            <StyledTab label="Saved" value="/saved" component={Link} to={allTabs[2]} />
+                                                            <StyledTab label="My Account" onClick={handleClick} />
+                                                            <AccountMenu anchorEl={anchorEl} onClose={handleClose} setDarkMode={setDarkMode} darkMode={darkMode}/>
+                                                        </StyledTabs>
+                                                    </div>
+                                                </Grid>
+                                            </Grid>}
                                         <Switch>
                                             <Route path={allTabs[1]} render={() => <Revise/>} />
                                             <Route path={allTabs[2]} render={() => <Saved/>} />
--- src/pages/revise.js
+++ src/pages/revise.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import Grid from '@material-ui/core/Grid';
 import FilterBox from '../components/FilterBox';
 import Flashcard from '../components/Flashcard';
 import HotkeyBox from "../components/HotkeyBox";
+import { useMediaQuery } from 'react-responsive'
+import Fab from "@material-ui/core/Fab";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import Dialog from "@material-ui/core/Dialog";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles( theme =>({
+    filterButton: {
+        position: 'absolute',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    }
+}))
 
 function Revise() {
-
+    const classes = useStyles();
     const [isRendered, setIsRendered] = React.useState(true);
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1280px)' })
+    const [open, setOpen] = useState(false);
+
+    function SimpleDialog(props) {
+        const { onClose, selectedValue, open } = props;
+
+        const handleClose = () => {
+            onClose(selectedValue);
+        };
+
+        return (
+            <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+                <FilterBox/>
+            </Dialog>
+        );
+    }
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
 
     return (
         <div>
@@ -18,7 +51,7 @@ function Revise() {
                 <Grid container item justify="center">
                     <Grid container item md={12} spacing={5} justify="center">
                         <Grid container item lg={4} xl={3} justify="center">
-                            <FilterBox/>
+                            {!isTabletOrMobile && <FilterBox/>}
                         </Grid>
 
                         <Grid container item lg={8} xl={9}>
@@ -26,7 +59,7 @@ function Revise() {
                             <Grid item xs={12}>
                                <Flashcard setIsRendered={setIsRendered}/>
                             </Grid>
-                            {isRendered &&
+                            {isRendered && !isTabletOrMobile &&
                                 <Grid item xs={12}>
                                     <HotkeyBox/>
                                 </Grid>
@@ -34,6 +67,11 @@ function Revise() {
                         </Grid>
                     </Grid>
                 </Grid>
+                {isTabletOrMobile &&
+                <Fab className={classes.filterButton} onClick={handleClick}>
+                    <FilterListIcon/>
+                </Fab>}
+                <SimpleDialog open={open} onClose={handleClick} />
             </Grid>
         </div>
 
