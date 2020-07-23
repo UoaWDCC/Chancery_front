4dce66f9d2f892e1594b55afa2317208f5878fa6
M-Stable <fchanghlee@gmail.com>
2020-07-23 18:51:17 +1200

    Hotkey box stays Adjust Revise page

--- src/pages/revise.js
+++ src/pages/revise.js
@@ -6,29 +6,24 @@ import HotkeyBox from "../components/HotkeyBox";
 
 function Revise() {
 
-    const [isRendered, setIsRendered] = React.useState(true);
-
     return (
         <div style={{ position: "relative", paddingTop: '10vh' }}>
             <Grid
                 container
-                spacing={0}
-                style={{ minHeight: '90vh', padding: '3em 5em 0em 5em'}}
+                style={{ minHeight: '90vh', padding: '3em 7em 0em 7em'}}
                 justify={"center"}
             >
-                <Grid container item md={12}>
-                    <Grid container item lg={4} xl={3} justify="center">
+                <Grid container item md={12} >
+                    <Grid container item lg={4} xl={3} justify={"center"}>
                         <FilterBox/>
                     </Grid>
-                    <Grid container item lg={8} xl={9}>
-                        <Grid item xs={12}>
-                            <Flashcard setIsRendered={setIsRendered}/>
+                    <Grid container direction={"column"} item lg={8} xl={9} alignItems={"center"}>
+                        <Grid item>
+                            <Flashcard/>
                         </Grid>
-                        {isRendered &&
-                        <Grid item xs={12}>
+                        <Grid item>
                             <HotkeyBox/>
                         </Grid>
-                        }
                     </Grid>
                 </Grid>
             </Grid>
