afc06f8ab9c711a1902e4323447c10c8d4636c6b
Steven Ho <kuanchenho@gmail.com>
2020-07-30 02:02:00 +1200

    Remove dummy flashcard from reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,11 +2,11 @@ import * as constants from './constants';
 
 export const initialState = {
     flashcards: [{
-        question:"Walk me through the 3 financial statements",
-        answer:"The 3 major financial statements are the Income Statement, Balance Sheet and Cash Flow Statement.\nThe Income Statement gives the company's revenue and expenses, and goes down to Net Income, the final line on the statement.\nThe Balance Sheet shows the company's Assets - its resources - such as Cash, Inventory and PP&E, as well as its Liabilities - such as Debt and Accounts Payable - and Shareholders' Equity. Assets must equal Liabilities plus Shareholders' Equity.", 
-        difficulty:"Easy",
-        topic:"Accounting", 
-        id:"1",
+        question:"",
+        answer:"", 
+        difficulty:"",
+        topic:"", 
+        id:"",
     }],
     loading: true,
     displayedFlashcards: [],
