a0ef33798ff988ea7e8abb0d8ee5b0fd24003511
Steven Ho <kuanchenho@gmail.com>
2020-08-02 18:00:16 +1200

    [CHNCY-020] Updated consistency when swapping between tabs (#40)
    * Updated consistency when swapping between tabs
    
    * Merged two useEffects that had the same functionality into one

--- src/redux/epic.js
+++ src/redux/epic.js
@@ -1,27 +1,71 @@
-import { combineEpics } from 'redux-observable';
-import { mergeMap, map } from 'rxjs/operators';
-import { getFlashcards } from '../api/chanceryApi';
-import * as constants from './constants';
-import {fetchQuestionsSuccess, updateFiltersSuccess} from './actions';
+import { combineEpics } from "redux-observable";
+import { mergeMap, map } from "rxjs/operators";
+import { getFlashcards } from "../api/chanceryApi";
+import * as constants from "./constants";
+import {
+  fetchQuestionsSuccess,
+  updateFiltersSuccess,
+  resetFiltersSuccess,
+  updateCurrentIndexSuccess,
+} from "./actions";
+import allFalse from "../helperFunctions/allFalse";
+import shuffle from "../helperFunctions/shuffle";
 
 export const fetchQuestionsEpic = (action$, state$) =>
-action$.ofType(constants.FETCH_QUESTIONS).pipe(
+  action$.ofType(constants.FETCH_QUESTIONS).pipe(
     mergeMap(async () => {
-        const result = await getFlashcards();
-        return fetchQuestionsSuccess(result);
-    })    
-);
+      const result = await getFlashcards();
+      shuffle(result);
+      return fetchQuestionsSuccess(result);
+    })
+  );
 
 export const updateFiltersEpic = (action$, state$) =>
-action$.ofType(constants.UPDATE_FILTERS).pipe(
-    map( action => {
-        const flashcardsByTopic = state$.value.flashcards.filter(flashcard => action.payload.topics.length === 0 || action.payload.topics.includes(flashcard.topic));
-        const filteredFlashcardsId = flashcardsByTopic.filter(flashcard => action.payload.difficulties.length === 0 || action.payload.difficulties.includes(flashcard.difficulty)).map(flashcard => flashcard.id);
-        return updateFiltersSuccess(filteredFlashcardsId);
+  action$.ofType(constants.UPDATE_FILTERS).pipe(
+    map((action) => {
+      const flashcardsByTopic = state$.value.flashcards.filter(
+        (flashcard) =>
+          allFalse(action.payload.topics) ||
+          action.payload.topics[flashcard.topic]
+      );
+      const filteredFlashcards = flashcardsByTopic.filter(
+        (flashcard) =>
+          allFalse(action.payload.difficulties) ||
+          action.payload.difficulties[flashcard.difficulty]
+      );
+      shuffle(filteredFlashcards);
+
+      return updateFiltersSuccess(
+        filteredFlashcards,
+        action.payload.topics,
+        action.payload.difficulties
+      );
     })
-);
+  );
+
+export const resetFiltersEpic = (action$, state$) =>
+  action$.ofType(constants.RESET_FILTERS).pipe(
+    map(() => {
+      const flashcards = state$.value.flashcards;
+      shuffle(flashcards);
+      return resetFiltersSuccess(flashcards);
+    })
+  );
+
+export const updateCurrentIndexEpic = (action$, state$) =>
+  action$.ofType(constants.UPDATE_CURRENT_INDEX).pipe(
+    map((action) => {
+      const currentIndex =
+        (action.payload + state$.value.displayedFlashcards.length) %
+        state$.value.displayedFlashcards.length;
+
+      return updateCurrentIndexSuccess(currentIndex);
+    })
+  );
 
 export const rootEpic = combineEpics(
-    fetchQuestionsEpic,
-    updateFiltersEpic,
+  fetchQuestionsEpic,
+  updateFiltersEpic,
+  resetFiltersEpic,
+  updateCurrentIndexEpic
 );
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,25 +1,58 @@
-import * as constants from './constants';
+import * as constants from "./constants";
+
+let initialDifficulties = {};
+let initialTopics = {};
+
+constants.topics.forEach((topic) => {
+  initialTopics[topic] = false;
+});
+
+constants.difficulties.forEach((difficulty) => {
+  initialDifficulties[difficulty] = false;
+});
 
 export const initialState = {
-    flashcards: [],
-    displayedFlashcards: [],
+  flashcards: [],
+  displayedFlashcards: [],
+  difficulties: initialDifficulties,
+  topics: initialTopics,
+  currentIndex: 0,
 };
 
 export const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case constants.FETCH_QUESTIONS_SUCCESS:
-            return {
-                ...state, 
-                flashcards: action.payload,
-            };
+  switch (action.type) {
+    case constants.FETCH_QUESTIONS_SUCCESS:
+      return {
+        ...state,
+        flashcards: action.payload,
+        displayedFlashcards: action.payload,
+      };
 
-        case constants.UPDATE_FILTERS_SUCCESS:
-            return {
-                ...state, 
-                displayedFlashcards: action.payload,
-            }
+    case constants.UPDATE_FILTERS_SUCCESS:
+      return {
+        ...state,
+        displayedFlashcards: action.payload.flashcards,
+        difficulties: action.payload.difficulties,
+        topics: action.payload.topics,
+        currentIndex: 0,
+      };
 
-        default:
-            return state;
-    }
+    case constants.RESET_FILTERS_SUCCESS:
+      return {
+        ...state,
+        difficulties: initialDifficulties,
+        topics: initialTopics,
+        displayedFlashcards: action.payload,
+        currentIndex: 0,
+      };
+
+    case constants.UPDATE_CURRENT_INDEX_SUCCESS:
+      return {
+        ...state,
+        currentIndex: action.payload,
+      };
+
+    default:
+      return state;
+  }
 };
