7cb704a7543141aaa886d0fd545f06d1f753d100
justinkrojos <justinkrojos@gmail.com>
2020-08-04 14:52:17 +1200

    [CHNCY-009] Fixed tag wrapping issue

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -6,18 +6,25 @@ import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(() => ({
     container: {
+        position: 'relative',
         backgroundColor: "#21CE99",
         borderRadius: 5,
-        padding: '4px 15px 4px 10px',
-        margin: 5,
+        padding: '5px 15px 5px 10px',
+        margin: '5px',
+        minHeight: '30px',
     },
     text: {
         color: "white",
         fontSize: "18px",
         textTransform: "uppercase",
-        float: "left",
+        lineHeight: '30px',
+        textAlign: 'left',
+        marginLeft: '25px',
     },
     icon: {
+        position: 'absolute',
+        top: '9px',
+        fontSize: 20,
         fontSize: 18,
         color: "white",
         paddingRight: 5,
@@ -29,7 +36,7 @@ function Tag(props) {
 
     return (
         <div className={classes.container}>
-            <Grid container alignItems={"center"}>
+            <Grid container>
                 <LocalOfferIcon className={classes.icon} />
                 <Typography id="topic" className={classes.text}>
                     {props.text}
