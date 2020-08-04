7d449bde2b381e610993fbdcc48464f27405a7fb
justinkrojos <justinkrojos@gmail.com>
2020-08-04 15:14:35 +1200

    [CHNCY-009] Changed hotkey description

--- src/components/HotkeyBox.js
+++ src/components/HotkeyBox.js
@@ -69,7 +69,7 @@ function HotkeyBox() {
 
           <Typography className={classes.information}>
             <img src={Save} className={"hotkey-icon"} alt="Save Key"/>
-            <b>Save </b>- Press the S key to save a question
+            <b>Save </b>- S key to save your questions for later
           </Typography>
         </Grid>
       </Grid>
