ebf1ebb41f9249689e4de3e693127e003815118c
Yifan Ai <ayfallen@hotmail.com>
2020-11-05 18:04:15 +1100

    Remove excessive padding on flash cards

--- src/pages/revise.js
+++ src/pages/revise.js
@@ -8,7 +8,7 @@ import {makeStyles} from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
     root: {
         minHeight: '100vh' - theme.spacing(5),
-        padding: '102px 7em 0em 7em',
+        padding: '102px 1em 0em',
 		maxWidth: '2560px',
 		margin: '0 auto',
     }
