f751d6183654eff5028f1e774d5ee3fbec8f7c84
richard875 <richard_875@me.com>
2020-12-14 00:04:57 +1300

    - prittier plugin for vscode auto-format

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,53 +1,52 @@
-import React from "react"
+import React from "react";
 import Grid from "@material-ui/core/Grid";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(() => ({
-    container: {
-        position: 'relative',
-        backgroundColor: "#21CE99",
-        borderRadius: 5,
-        padding: '5px 15px 5px 10px',
-        margin: '5px',
-        height: '30px',
-		maxWidth: '100%',
-    },
-    text: {
-        color: "white",
-        fontSize: "18px",
-        textTransform: "uppercase",
-        lineHeight: '30px',
-        textAlign: 'left',
-        marginLeft: '25px',
-		overflow: 'hidden',
-		textOverflow: 'ellipsis',
-		whiteSpace: 'nowrap',
-    },
-    icon: {
-        position: 'absolute',
-        top: '9px',
-        fontSize: 20,
-        color: "white",
-        paddingRight: 5,
-    }
+  container: {
+    position: "relative",
+    backgroundColor: "#21CE99",
+    borderRadius: 5,
+    padding: "5px 15px 5px 10px",
+    margin: "5px",
+    height: "30px",
+    maxWidth: "100%",
+  },
+  text: {
+    color: "white",
+    fontSize: "18px",
+    textTransform: "uppercase",
+    lineHeight: "30px",
+    textAlign: "left",
+    marginLeft: "25px",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+  },
+  icon: {
+    position: "absolute",
+    top: "9px",
+    fontSize: 20,
+    color: "white",
+    paddingRight: 5,
+  },
 }));
 
 function Tag(props) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.container}>
-            <Grid container>
-                <LocalOfferIcon className={classes.icon} />
-                <Typography id="topic" className={classes.text}>
-                    {props.text}
-                </Typography>
-            </Grid>
-        </div>
-
-    )
+  return (
+    <div className={classes.container}>
+      <Grid container>
+        <LocalOfferIcon className={classes.icon} />
+        <Typography id="topic" className={classes.text}>
+          {props.text}
+        </Typography>
+      </Grid>
+    </div>
+  );
 }
 
-export default Tag
+export default Tag;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import configureStore from './redux/configureStore';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
 
 const store = configureStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
