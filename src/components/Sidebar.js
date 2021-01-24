5e8c7965348235afe92a656a569add2a95937ae6
Wayne Yao <wayneyao99@gmail.com>
2021-01-24 22:50:59 +1300

    add router into sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,43 +2,55 @@ import React from "react";
 import { stack as Menu } from "react-burger-menu";
 import "./Sidebar.css";
 import AccountMenu from "../components/AccountMenu";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import Home from "../pages/home";
+import Revise from "../pages/revise";
+import Saved from "../pages/saved";
 
 function Sidebar(props) {
   const { darkMode, setDarkMode, handleClick, handleClose, anchorEl } = props;
   return (
-    <Menu {...props}>
-      <a className="menu-item" href="/">
-        Home
-      </a>
+    <BrowserRouter>
+      <Menu {...props}>
+        <Link className="menu-item" to="/">
+          Home
+        </Link>
 
-      <a className="menu-item" href="/revise">
-        Revise
-      </a>
+        <Link className="menu-item" to="/revise">
+          Revise
+        </Link>
 
-      <a className="menu-item" href="/saved">
-        Saved
-      </a>
+        <Link className="menu-item" to="/saved">
+          Saved
+        </Link>
 
-      <button
-        className="menu-item"
-        onClick={handleClick}
-        style={{
-          fontSize: "23px",
-          background: "transparent",
-          border: "none",
-          textAlign: "left",
-        }}
-      >
-        My Account
-      </button>
+        <button
+          className="menu-item"
+          onClick={handleClick}
+          style={{
+            fontSize: "23px",
+            background: "transparent",
+            border: "none",
+            textAlign: "left",
+          }}
+        >
+          My Account
+        </button>
 
-      <AccountMenu
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        setDarkMode={setDarkMode}
-        darkMode={darkMode}
-      />
-    </Menu>
+        <AccountMenu
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          setDarkMode={setDarkMode}
+          darkMode={darkMode}
+        />
+      </Menu>
+
+      <Switch>
+        <Route path={"/revise"} render={() => <Revise />} />
+        <Route path={"/saved"} render={() => <Saved />} />
+        <Route path={"/"} render={() => <Home />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
