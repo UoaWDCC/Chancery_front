06c5f88461797d4e2cd4d253e5b240937db8cc4a
Wayne Yao <wayneyao99@gmail.com>
2021-01-26 18:12:41 +1300

    Get rid of all eslint and console warnings

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,55 +2,44 @@ import React from "react";
 import { stack as Menu } from "react-burger-menu";
 import "./Sidebar.css";
 import AccountMenu from "../components/AccountMenu";
-import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
-import Home from "../pages/home";
-import Revise from "../pages/revise";
-import Saved from "../pages/saved";
+import { Link } from "react-router-dom";
 
 function Sidebar(props) {
   const { darkMode, setDarkMode, handleClick, handleClose, anchorEl } = props;
   return (
-    <BrowserRouter>
-      <Menu {...props}>
-        <Link className="menu-item" to="/">
-          Home
-        </Link>
+    <Menu {...props}>
+      <Link className="menu-item" to="/">
+        Home
+      </Link>
 
-        <Link className="menu-item" to="/revise">
-          Revise
-        </Link>
+      <Link className="menu-item" to="/revise">
+        Revise
+      </Link>
 
-        <Link className="menu-item" to="/saved">
-          Saved
-        </Link>
+      <Link className="menu-item" to="/saved">
+        Saved
+      </Link>
 
-        <button
-          className="menu-item"
-          onClick={handleClick}
-          style={{
-            fontSize: "23px",
-            background: "transparent",
-            border: "none",
-            textAlign: "left",
-          }}
-        >
-          My Account
-        </button>
+      <button
+        className="menu-item"
+        onClick={handleClick}
+        style={{
+          fontSize: "23px",
+          background: "transparent",
+          border: "none",
+          textAlign: "left",
+        }}
+      >
+        My Account
+      </button>
 
-        <AccountMenu
-          anchorEl={anchorEl}
-          onClose={handleClose}
-          setDarkMode={setDarkMode}
-          darkMode={darkMode}
-        />
-      </Menu>
-
-      <Switch>
-        <Route path={"/revise"} render={() => <Revise />} />
-        <Route path={"/saved"} render={() => <Saved />} />
-        <Route path={"/"} render={() => <Home />} />
-      </Switch>
-    </BrowserRouter>
+      <AccountMenu
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        setDarkMode={setDarkMode}
+        darkMode={darkMode}
+      />
+    </Menu>
   );
 }
 
