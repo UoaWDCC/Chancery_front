addaccd615d83cce24e9d33c8c618f573587f8b2
Hakusen-git <63892309+Hakusen-git@users.noreply.github.com>
2021-03-07 14:09:05 +1300

    Solve unsave cors problem (#75)
    * Add contenttype to delete method so cors can allow the api call
    
    * Remove proxyurl

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,5 +1,7 @@
 const API_ROOT = `https://o6xaaxitq4.execute-api.ap-southeast-2.amazonaws.com/prod`;
 
+// const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
 export const postUserInfo = async (user) => {
   try {
     await fetch(`${API_ROOT}/users/post/`, {
@@ -27,6 +29,9 @@ export const deleteBookmark = async (bookmark) => {
     await fetch(`${API_ROOT}/users/bookmark/unsave/`, {
       method: "DELETE",
       body: JSON.stringify(bookmark),
+      headers: {
+        "Content-Type": "application/json",
+      }
     }).then((result) => result.json());
   } catch (error) {
     console.log(error);
